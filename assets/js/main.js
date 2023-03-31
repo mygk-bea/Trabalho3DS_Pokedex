3f14368ab0f2b00da20f575c2020799a054b7630
lucas-r-nogueira <ramalhol148@gmail.com>
2023-03-31 19:29:40 -0300

    feature download API

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Input Nome do pokemon e botão
+const pokemon = document.getElementById("nav-input");
+const btn = document.getElementById("nav-input-btn");
+
+const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+btn.addEventListener(url){
+    
+}

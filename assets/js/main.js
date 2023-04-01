61b25e5ee06f846bc25f32b3517da42a38136e9c
mygk-bea <meyaguskobeatriz@gmail.com>
2023-03-31 22:06:43 -0300

    fix acesso ao JSON

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,15 @@
 // Input Nome do pokemon e botão
-const pokemon = document.getElementById("a");
+const pokemon = document.getElementById("input-text");
 const btn = document.querySelector(".nav__input-btn");
 
-const url = `https://pokeapi.co/api/v2/pokemon/${pokemon.value}`;
 
-btn.addEventListener("click",()=>{
-    console.log(pokemon.value + " console.log")
-    fetch(url)
-    .then(response => response.json())
-    .then(dados => { console.log(dados.species) })
-	.catch((_) => { console.log(_) })
+btn.addEventListener("click",() => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemon.value}`;
+
+  fetch(url)
+  .then(response => response.json())
+  .then(dados => { 
+    console.log(dados.species) 
+  })
+	.catch((_) => {console.log(_)})
 });

00433a5fc56df0e82f1ac216af26dddbac26837f
lucas-r-nogueira <ramalhol148@gmail.com>
2023-03-31 21:12:29 -0300

    feature aparecer JSON

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 // Input Nome do pokemon e botão
-const pokemon = document.getElementById("nav-input");
-const btn = document.getElementById("nav-input-btn");
+const pokemon = document.getElementById("a");
+const btn = document.querySelector(".nav__input-btn");
 
-const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+const url = `https://pokeapi.co/api/v2/pokemon/${pokemon.value}`;
 
-btn.addEventListener(url){
-    
-}
+btn.addEventListener("click",()=>{
+    console.log(pokemon.value + " console.log")
+    fetch(url)
+    .then(response => response.json())
+    .then(dados => { console.log(dados.species) })
+	.catch((_) => { console.log(_) })
+});

10779144f68974ec8f2aed8cba0f0e895c3d3c82
lucas-r-nogueira <ramalhol148@gmail.com>
2023-03-31 23:52:37 -0300

    feature Aparecer imagens e dados

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,41 @@
 const pokemon = document.getElementById("input-text");
 const btn = document.querySelector(".nav__input-btn");
 
+let campos = {
+    
+}
 
 btn.addEventListener("click",() => {
-  const url = `https://pokeapi.co/api/v2/pokemon/${pokemon.value}`;
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemon.value.toLowerCase()}`;
+  const div_img = document.querySelector(".content__pokemon-img");
 
   fetch(url)
   .then(response => response.json())
   .then(dados => { 
-    console.log(dados.species) 
+    console.log(dados)
+
+    console.log("Nome " + dados.species.name) 
+    // For para o selecionamento de Atributos dos arrays
+    for(let i = 0; i < dados.abilities.length; i++){
+        console.log("Habilidades " + dados.abilities[i].ability.name)
+    }
+    
+    for(let i = 0; i < dados.forms.length; i++){
+        console.log("Forma " + dados.forms[i].name)
+    }
+
+    for(let i = 0; i < dados.types.length; i++){
+        console.log("Tipos " + dados.types[i].type.name)
+    }
+
+    console.log("Especie " + dados.species.name)
+    console.log("Experiencia " + dados.base_experience)
+    console.log("Altura " + dados.height)
+
+    // img
+    let img = document.createElement('img');
+    img.setAttribute('src', dados.sprites.front_default);
+    div_img.appendChild(img)
   })
 	.catch((_) => {console.log(_)})
 });
